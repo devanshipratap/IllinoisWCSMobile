8f183041e1404da1c82bf587895e8271fb5cde4f
Devanshi Pratap <dpratap2@illinois.edu>
2020-04-11 22:14:51 -0500

    creating login component

--- front-end/App.js
+++ front-end/App.js
@@ -1,65 +1,18 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, Image, Button  } from 'react-native';
-import * as Google from 'expo-google-app-auth';
-import { clientId } from './auth.json';
+import Login from './components/Login.js'
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-      this.state = {
-        signedIn: false,
-        name: "",
-      }
-  }
-
-  signIn = async () => {
-    try {
-      const result = await Google.logInAsync({
-        iosClientId: clientId,
-        scopes: ['profile', 'email'],
-      });
-
-      if (result.type === 'success') {
-        this.setState({
-          signedIn: true,
-          name: result.user.name,
-        })
-      } else {
-        console.log("Request cancelled")
-      }
-    } catch (e) {
-      console.log("Error", e)
-    }
-  };
 
   render() {
     return (
         <View style={styles.container}>
-        {this.state.signedIn
-              ? ( <LoggedInPage name={this.state.name} />)
-              : ( <LoginPage signIn={this.signIn} /> )}
+          <Login/>
         </View>
     )
   }
 }
 
-const LoginPage = props => {
-  return (
-      <View >
-        <Image style={styles.image} source={require('./illinoiswcs.png')}/>
-        <Button style={styles.header} title="Sign in with Google" onPress={() => props.signIn()} />
-      </View>
-      )
-};
-
-const LoggedInPage = props => {
-  return (
-      <View>
-        <Image style={styles.image} source={require('./illinoiswcs.png')}/>
-        <Text style={styles.header}>Welcome! {props.name}</Text>
-  </View>
-)
-};
 
 const styles = StyleSheet.create({
   container: {
---
+++ front-end/components/Login.js
@@ -0,0 +1,84 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Image, Button  } from 'react-native';
+import * as Google from 'expo-google-app-auth';
+import { clientId } from '../auth.json';
+
+export default class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            signedIn: false,
+            name: "",
+        }
+    }
+
+    signIn = async () => {
+        try {
+            const result = await Google.logInAsync({
+                iosClientId: clientId,
+                scopes: ['profile', 'email'],
+            });
+
+            if (result.type === 'success') {
+                this.setState({
+                    signedIn: true,
+                    name: result.user.name,
+                })
+            } else {
+                console.log("Request cancelled")
+            }
+        } catch (e) {
+            console.log("Error", e)
+        }
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                {this.state.signedIn
+                    ? ( <LoggedInPage name={this.state.name} />)
+                    : ( <LoginPage signIn={this.signIn} /> )}
+            </View>
+        )
+    }
+}
+
+const LoginPage = props => {
+    return (
+        <View >
+            <Image style={styles.image} source={require('../illinoiswcs.png')}/>
+            <Button style={styles.header} title="Sign in with Google" onPress={() => props.signIn()} />
+        </View>
+    )
+};
+
+const LoggedInPage = props => {
+    return (
+        <View>
+            <Image style={styles.image} source={require('../illinoiswcs.png')}/>
+            <Text style={styles.header}>Welcome! {props.name}</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#ffff",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    header: {
+        fontSize: 24,
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 0.2
+    },
+    image: {
+        marginTop: 15,
+        width: 210,
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 0.25
+    }
+});
